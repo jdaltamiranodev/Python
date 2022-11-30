7a27b8153e2820698a77a60971534e2de7ff34d8
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-30 09:58:33 -0500

    Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.

---
+++ Ejercicio/Ejercicio-4/main.py
@@ -0,0 +1,12 @@
+# Escribe un programa que sea capaz de mostrar los números del 1 al 10
+# Cuando comiences el ejercicio se te mostrarán las especificaciones para la realización y entrega del mismo. La entrega del ejercicio deberá realizarse a través de una carpeta .zip empaquetada o un enlace al repositorio del ejercicio en GitHub.
+#
+#
+# Una vez le des al botón de comenzar ejercicio el tiempo del que dispones para realizarlo empezará a contar.
+#
+#
+# Asegúrate de tener todo lo que necesites preparado ya que una vez se acabe el tiempo si no has realizado la entrega esta contará como no superada.
+#
+
+for i in range(100, 0, -1):
+    print(i)
